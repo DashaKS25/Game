7a7588c761097ecec029766c81c1bfb8bfc5e7d1
galaida <galaida.arseniy@gmail.com>
2023-05-11 15:39:24 +0300

    Some updates

--- Game.py
+++ Game.py
@@ -1,38 +1,43 @@
-import random
-words = ['python', 'programming', 'language', 'computer', 'disk', 'dock', 'data',]
-word = random.choice(words)
-guessed = []
-max_attempts = 6
-
-def display_word(word, guessed):
-    for letter in word:
-        if letter in guessed:
-            print(letter, end=' ')
-        else:
-            print('_', end=' ')
+def display_word(w, guessing):  # changing names, because word and guessed are global variables
+    for letter in w:
+        print(letter if letter in guessing else '_', end=' ')  # make print shorter
     print()
 
-def is_word_guessed(word, guessed):
-    for letter in word:
-        if letter not in guessed:
+
+def is_word_guessed(w, guessing):  # changing names, because word and guessed are global variables
+    for letter in w:
+        if letter not in guessing:
             return False
     return True
 
 
-while max_attempts > 0:
-    print(f'У вас {max_attempts} попыток.')
-    display_word(word, guessed)
-    guess = input('Введите букву: ').lower()
-    if guess in guessed:
-        print('Вы уже угадали эту букву!')
-    elif guess in word:
-        guessed.append(guess)
-        if is_word_guessed(word, guessed):
-            print(f'Вы победили! Загаданное слово было "{word}".')
-            break
+import random
+words = ['python', 'programming', 'language', 'computer', 'disk', 'dock', 'data', ]
+score = 0
+while True:
+    choice = input('You want to guess word? (y/n): ')
+    if choice.lower() == 'y' and len(words) > 0:
+        word = random.choice(words)
+        guessed = []
+        max_attempts = 6
+        while max_attempts > 0:
+            print(f'Your amount of tries: {max_attempts}.')
+            display_word(word, guessed)
+            guess = input('Enter letter: ').lower()
+            if guess in guessed:
+                print('You already asked this letter!')
+            elif guess in word:
+                guessed.append(guess)
+                if is_word_guessed(word, guessed):
+                    print(f'You won! Word was "{word}".')
+                    score += 1
+                    break
+            else:
+                print('There is no such letter in the hidden word.')
+                max_attempts -= 1
+        else:
+            print(f'You lost. Word was "{word}".')
+        words.remove(word)
     else:
-        print('Такой буквы нет в загаданном слове.')
-        max_attempts -= 1
-else:
-    print(f'Вы проиграли. Загаданное слово было "{word}".')
-
+        print(f'Thank for playing, your score: {score}.')
+        break
